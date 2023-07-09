4d19bae8c6c314ff4bcd92867522780471b0c727
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-09 08:53:19 +0300

    fix: fix some incorrect madia queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,12 +23,12 @@ const Navbar = () => {
       <nav className="flex flex-row sticky top-0 z-50 justify-between px-4 py-3 md:py-4 bg-background">
         <div className="flex flex-row items-center">
           <img src={Logo} alt="Logo image" className="h-10 w-10 scale-110" />
-          <span className="font-extrabold text-3xl ml-2">
+          <span className="font-extrabold text-3xl ml-2 hidden md:block">
             Ber<span className="text-gray-500 ml-1">AI</span>
           </span>
         </div>
 
-        <div className="flex-row hidden md:flex rounded-xl border-gray-200 border-2 p-2 font-medium text-gray-500 gap-4 absolute right-1/3 top-3">
+        <div className="flex-row hidden lg:flex rounded-xl border-gray-200 border-2 p-2 font-medium text-gray-500 gap-4 absolute right-1/3 top-3">
           <button className="tracking-tight px-4 bg-black rounded-xl text-white p-2">
             Dashboard
           </button>
@@ -50,7 +50,7 @@ const Navbar = () => {
           </button>
 
           <button
-            className="hover:bg-gray-200 rounded-xl p-2 ml-2 md:hidden"
+            className="hover:bg-gray-200 rounded-xl p-2 ml-2 lg:hidden"
             onClick={() => setIsOpen(!isOpen)}
           >
             {!isOpen ? (
